d0a522671062bb09e4dcf82574457a039948f047
Matthew Harrison <Matthew.Harrison@noaa.gov>
2017-04-07 10:51:56 -0400

    my updates for running gnu on Mint

--- templates/linux-gnu.mk
+++ templates/linux-gnu.mk
@@ -21,8 +21,9 @@ MAKEFLAGS += --jobs=$(shell grep '^processor' /proc/cpuinfo | wc -l)
 
 FPPFLAGS :=
 
-FFLAGS := -fcray-pointer -fdefault-double-8 -fdefault-real-8 -Waliasing -ffree-line-length-none -fno-range-check
-FFLAGS += -I$(shell nc-config --includedir)
+FFLAGS := -fcray-pointer -fdefault-double-8 -fdefault-real-8 -Waliasing -ffree-line-length-none -fno-range-check --std=gnu
+#FFLAGS += -I$(shell nc-config --includedir)
+FFLAGS += -I/usr/include -I/usr/local/include
 FFLAGS += $(shell pkg-config --cflags-only-I mpich2-c)
 FFLAGS_OPT = -O3
 FFLAGS_REPRO = -O2 -fbounds-check
