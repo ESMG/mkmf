fe6d5f1273204b0f3d21d0fca59db9e864db7e4b
Kate Hedstrom <kshedstrom@alaska.edu>
2019-07-09 09:50:24 -0800

    Getting rid of a few trailing spaces.

--- templates/macOS-gnu8-mpich3.mk
+++ templates/macOS-gnu8-mpich3.mk
@@ -1,4 +1,4 @@
-# Template for the GNU Compiler on macOS 
+# Template for the GNU Compiler on macOS
 # Tested on macOS Mojave (version 10.14.2), with gnu 8.2 and mpich 3.3
 #
 # Typical use with mkmf
@@ -81,7 +81,7 @@ $(error Options DEBUG and TEST cannot be used together)
 endif
 endif
 
-# Get number of CPUs 
+# Get number of CPUs
 #MAKEFLAGS += --jobs=$(shell grep '^processor' /proc/cpuinfo | wc -l)
 MAKEFLAGS += --jobs=$(shell sysctl -n hw.ncpu)
 # Macro for Fortran preprocessor
